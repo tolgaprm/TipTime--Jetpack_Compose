776853e5360203461ff76879f1da19805c165979
Tolga Pirim <tolgapirim25@gmail.com>
2021-09-24 11:26:20 +0300

    fixed some issues

--- app/src/main/java/com/tolgapirim/tiptime/MainActivity.kt
+++ app/src/main/java/com/tolgapirim/tiptime/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.tolgapirim.tiptime
 
+import android.inputmethodservice.Keyboard
+import android.media.Image
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Switch
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -21,6 +27,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tolgapirim.tiptime.ui.theme.TipTimeTheme
+import org.w3c.dom.Text
 import java.text.NumberFormat
 
 
@@ -28,104 +35,110 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-          MyApp()
-        }
-    }
-}
+            TipTimeTheme() {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            {
+                                Text(
+                                    text = "Tip Time",
+                                    fontSize = 20.sp,
 
-@Composable
-fun MyApp(){
-    TipTimeTheme() {
-        Scaffold(
-            topBar = {  TopAppBar(
-                {
-                    Text(
-                        text = "Tip Time",
-                        fontSize = 20.sp,
-
-                    )
-                }
-            )
+                                    )
+                            }
+                        )
 
 
-
-        }
-        ) {
-            var costOfService by remember { mutableStateOf("")}
-            val tipPercentage:MutableState<Double> = remember { mutableStateOf(0.20)}
-            val selected:MutableState<Boolean> = remember { mutableStateOf(true)}
-            val formattedTip = remember { mutableStateOf("")}
-            Column(modifier = Modifier.padding(16.dp) ) {
-
-                // CostOfService EditText
-                Row(verticalAlignment = Alignment.CenterVertically,modifier = Modifier.padding(bottom = 30.dp)) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_baseline_store_24),
-                        contentDescription =null,
-                        modifier = Modifier
-                            .size(40.dp)
-                            .padding(end = 10.dp)
-                    )
-
-                    OutlinedTextField(
-                        value = costOfService,
-                        onValueChange ={
-                            costOfService = it
-                        },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                        label = { Text(text = "Cost of Service")}
-                    
-                    )
-                
-                    
-
-                }
-
-                HowWasTheService()
-
-                // Radio Button
-                RadioGroup(tipPercentage)
-
-
-                // Switch Button
-                RoundUpTip(selected)
-
-
-                // Calculate Button
-                Button(
-                    onClick = {
-                         formattedTip.value = calculateTipPercantage(costOfService,tipPercentage,selected)
-                    },
-                    modifier = Modifier
-                        .padding(top = 20.dp, start = 45.dp, end = 0.dp)
-                        .fillMaxWidth()
-                        .height(40.dp)
-
+                    }
                 ) {
-                    Text(text = "CALCULATE")
+                    MyApp()
                 }
-
-
-                // Tip Amount Text
-                Text(
-                    text =  "Tip Amount: ${formattedTip.value}",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 20.dp, end = 5.dp)
-                        .alpha(0.5f),
-                    textAlign = TextAlign.End
-                )
             }
         }
     }
 }
 
+@Composable
+fun MyApp() {
+
+    var costOfService by remember { mutableStateOf("") }
+    val tipPercentage: MutableState<Double> = remember { mutableStateOf(0.20) }
+    val selected: MutableState<Boolean> = remember { mutableStateOf(true) }
+    val formattedTip = remember { mutableStateOf("") }
+    Column(modifier = Modifier.padding(16.dp)) {
+
+        // CostOfService EditText
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(bottom = 30.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_baseline_store_24),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                    .padding(end = 10.dp)
+            )
+
+            OutlinedTextField(
+                value = costOfService,
+                onValueChange = {
+                    costOfService = it
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                label = { Text(text = "Cost of Service") }
+
+            )
+
+
+        }
+
+        HowWasTheService()
+
+        // Radio Button
+        RadioGroup(tipPercentage)
+
+
+        // Switch Button
+        RoundUpTip(selected)
+
+
+        // Calculate Button
+        Button(
+            onClick = {
+                formattedTip.value =
+                    calculateTipPercantage(costOfService, tipPercentage, selected)
+            },
+            modifier = Modifier
+                .padding(top = 20.dp, start = 45.dp, end = 0.dp)
+                .fillMaxWidth()
+                .height(40.dp)
+
+        ) {
+            Text(text = "CALCULATE")
+        }
+
+
+        // Tip Amount Text
+        Text(
+            text = "Tip Amount: ${formattedTip.value}",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp, end = 5.dp)
+                .alpha(0.5f),
+            textAlign = TextAlign.End
+        )
+    }
+
+
+}
+
 
 @Composable
-fun HowWasTheService(){
+fun HowWasTheService() {
     Row(verticalAlignment = Alignment.CenterVertically) {
         Image(
-            painter = painterResource(id =R.drawable.ic_baseline_room_service_24 ),
+            painter = painterResource(id = R.drawable.ic_baseline_room_service_24),
             contentDescription = null,
             modifier = Modifier
                 .size(40.dp)
@@ -139,12 +152,12 @@ fun HowWasTheService(){
 
 
 @Composable
-fun RadioGroup(tipPercantage:MutableState<Double>) {
-    val radioOptions = listOf("Amazing (20%)","Good (18%)","Okay (15%)")
-    val (selectedOption,onOptionSelected) = remember { mutableStateOf(radioOptions[0])}
+fun RadioGroup(tipPercantage: MutableState<Double>) {
+    val radioOptions = listOf("Amazing (20%)", "Good (18%)", "Okay (15%)")
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[0]) }
 
     Column(modifier = Modifier.selectableGroup()) {
-        radioOptions.forEach {text->
+        radioOptions.forEach { text ->
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -165,11 +178,11 @@ fun RadioGroup(tipPercantage:MutableState<Double>) {
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 RadioButton(
-                    selected =(text == selectedOption),
+                    selected = (text == selectedOption),
                     onClick = {
                         onOptionSelected(text)
-                        println(text)
-                       tipPercantage.value = when (text) {
+
+                        tipPercantage.value = when (text) {
                             "Amazing (20%)" -> 0.20
                             "Good (18%)" -> 0.18
                             else -> 0.15
@@ -193,12 +206,15 @@ fun RadioGroup(tipPercantage:MutableState<Double>) {
 
 
 @Composable
-fun RoundUpTip(selected:MutableState<Boolean>){
+fun RoundUpTip(selected: MutableState<Boolean>) {
 
-    
-    Row(modifier = Modifier.padding(top = 20.dp), verticalAlignment = Alignment.CenterVertically) {
+
+    Row(
+        modifier = Modifier.padding(top = 20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Image(
-            painter = painterResource(id = R.drawable.ic_baseline_call_made_24) ,
+            painter = painterResource(id = R.drawable.ic_baseline_call_made_24),
             contentDescription = null,
             modifier = Modifier.size(40.dp)
         )
@@ -208,25 +224,24 @@ fun RoundUpTip(selected:MutableState<Boolean>){
         )
         Switch(
             checked = selected.value,
-            onCheckedChange ={selected.value = !selected.value},
+            onCheckedChange = { selected.value = !selected.value },
             modifier = Modifier.padding(start = 170.dp)
         )
 
 
-        
-       
     }
 }
 
 
-fun calculateTipPercantage(costOfService:String,
-                           tipPercantage:MutableState<Double>,
-                           selected:MutableState<Boolean>
-                            ):String {
-   var tip:Double = tipPercantage.value * costOfService.toDouble()
+fun calculateTipPercantage(
+    costOfService: String,
+    tipPercantage: MutableState<Double>,
+    selected: MutableState<Boolean>
+): String {
+    var tip: Double = tipPercantage.value * costOfService.toDouble()
 
-    if (selected.value==true){
-      tip = kotlin.math.ceil(tip)
+    if (selected.value == true) {
+        tip = kotlin.math.ceil(tip)
     }
 
     // Her ülkenin ayrı parabirimi ve bu parbirimlerinin gösterimleri farklı
@@ -242,6 +257,6 @@ fun calculateTipPercantage(costOfService:String,
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
- MyApp()
+    MyApp()
 }
 
